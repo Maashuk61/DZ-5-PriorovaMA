95a9cbec94f62767cdb4fc287ffb8c7dcae59980
Maria <mpriorova@mail.ru>
2023-01-22 18:29:11 +0300

    first commit

---
+++ 5_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150].
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+int[] RandomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for(int i = 0; i <array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void CountArray(int[] array)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array [j] >= 10 && array [j] <=99)  count = count + 1;
+    }
+    Console.WriteLine(count);
+}
+
+int [] massive = RandomArray(123, 0, 150);
+Console.WriteLine(string.Join(", ", massive));
+CountArray(massive);
